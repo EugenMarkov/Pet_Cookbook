e80403ed4b506611f4b03c9cf7bd6c00ae6253e6
Eugen Markov <seroglaziymail@gmail.com>
2020-03-21 14:14:09 +0200

    add ie11 polyfill fix stable

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -9,6 +10,7 @@ import Routes from "./routes/Routes";
 import { store } from "./store";
 import theme from "./theme";
 import "./index.css";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
