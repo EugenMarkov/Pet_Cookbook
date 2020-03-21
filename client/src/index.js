3addcd87a97c9b27d61543bdff6e62c2022d39dd
EugenMarkov <58852894+EugenMarkov@users.noreply.github.com>
2020-03-21 12:50:25 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import Routes from "./routes/Routes";
 import { store } from "./store";
 import theme from "./theme";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 import * as serviceWorker from "./serviceWorker";
 
